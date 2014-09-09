d667fcade8d531379cda2c695cd12646a8a34996
Kristo Kurtén <kristo.kurten@gmail.com>
2014-09-10 00:07:42 +0300

    Refactored to be more functional

--- src/rpg/core.clj
+++ src/rpg/core.clj
@@ -1,58 +1,47 @@
-(ns rpg.core
-  (:gen-class))
+(ns rpg.core)
 
-(def player {
-              :name "dzerome"
-              :hp 100
-              :weapon {
-                        :name "1h sword"
-                        :dmg 10}
-              }
-  )
-(def monsters [{:name "Spider" :hp 5 :dmg 5}
-               {:name "Goblin" :hp 10 :dmg 10}
-               {:name "Orc" :hp 20 :dmg 20}])
+(def player-character {:name "John Doe"
+                       :hp 100
+                       :weapon {:name "1h sword"
+                                :dmg 10}})
+(def monsters [{:name "Spider" :hp 20 :dmg 5}
+               {:name "Goblin" :hp 30 :dmg 10}
+               {:name "Orc" :hp 40 :dmg 20}])
 
-(def enemy nil)
-
-(defn player-alive? [] (pos? (:hp player)))
-
-(defn enemy-is-alive? []
-  (pos? (:hp enemy)))
-
-(defn take-dmg [dmg]
-  (def player (update-in player [:hp] - dmg)))
+(defn alive? [fighter]
+  (pos? (:hp fighter)))
 
 (defn random-enemy []
   (get monsters (rand-int (count monsters))))
 
 (defn random-dmg []
-  (-> player :weapon :dmg))
+  (-> player-character :weapon :dmg))
 
-(defn attack []
+(defn attack [enemy]
   (let [dmg (random-dmg)]
     (println (str "You hit enemy for " dmg "\n"))
-    (def enemy (update-in enemy [:hp] - dmg))))
+    (update-in enemy [:hp] - dmg)))
 
-(defn fight []
+(defn fight [player enemy]
   (println (str "Your hp:" (:hp player) "\t" "Enemy hp:" (:hp enemy) "\n"))
   (println "Press any key to attack the enemy")
   (read-line)
-  (attack))
+  (let [enemy (attack enemy)]
+    (if (alive? enemy)
+      (recur player enemy))))
 
-(defn random-fight []
-  (def enemy (random-enemy))
-  (println (str "You have encountered an " (:name enemy) "!\n"))
-  (while (enemy-is-alive?) (fight))
-  (println (str (:name enemy) " has died!\n"))
-  (def enemy nil)
-  (println "-----------------------------------------------"))
+(defn generate-fight [player]
+  (println "-----------------------------------------------")
+  (let [enemy (random-enemy)]
+    (println (str "You have encountered an " (:name enemy) "!\n"))
+    (fight player enemy)
+    (println (str (:name enemy) " has died!\n"))))
 
 (defn run-game []
-  (while true
-    (random-fight)))
+  (let [player player-character]
+    (while true
+      (generate-fight player))))
 
 (defn -main
   [& args]
-  (run-game)
-  )
+  (run-game))
