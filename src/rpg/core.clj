421459994bb46aa96e26e4ad23375a30f8a185de
Kristo Kurtén <kristo.kurten@gmail.com>
2014-09-10 18:37:43 +0300

    Implemented concept of a single battle with multiple generated enemies

--- src/rpg/core.clj
+++ src/rpg/core.clj
@@ -3,18 +3,29 @@
 (declare fight generate-fight)
 
 (def player {:name "John Doe"
-             :hp 100
+             :hp 1000
              :weapon {:name "1h sword"
-                      :dmg 10}})
-(def enemies [{:name "Spider" :hp 20 :dmg 5}
-              {:name "Goblin" :hp 30 :dmg 10}
-              {:name "Orc" :hp 40 :dmg 20}])
+                      :dmg 100}})
+(def enemy-types [{:name "Spider" :hp 50 :dmg 20}
+                  {:name "Blood elf" :hp 100 :dmg 30}
+                  {:name "Orc" :hp 200 :dmg 40}
+                  {:name "Fire elemental" :hp 150 :dmg 70}
+                  {:name "Darkhound" :hp 100 :dmg 100}
+                  {:name "Dreadlord" :hp 400 :dmg 150}])
+
+(defn create-enemy []
+  (get enemy-types (rand-int (count enemy-types))))
+
+(defn generate-enemies
+  ([count]
+    (generate-enemies count '()))
+  ([count enemies]
+    (if (zero? count)
+      enemies
+      (generate-enemies (dec count) (conj enemies (create-enemy))))))
 
 (defn alive? [fighter]
   (pos? (:hp fighter)))
-
-(defn random-enemy []
-  (get enemies (rand-int (count enemies))))
 
 (defn random-dmg []
   (-> player :weapon :dmg))
@@ -42,6 +53,24 @@
 (defn enemy-died [enemy]
   (println (str (:name enemy) " has died!\n")))
 
+(defn player-won? [player enemies]
+  (when (empty? enemies)
+    (println "\n****************************************************************************")
+    (println "\tCongratulations " (:name player) ", you have defeated all your enemies!")
+    (println "\tYou have" (:hp player) "hp left after the battle!")
+    (println "****************************************************************************\n")
+    (System/exit 0)))
+
+(defn show-fight-info [player enemy]
+  (println (str "You are attacked by an " (:name enemy) "!\n"))
+  (show-health player enemy))
+
+(defn show-intro [enemies]
+  (println "\nAs you enter the forest you are almost immediately attacked by multiple enemies!")
+  (println "You quickly assess the enemies you are facing: ")
+  (doall (map #(println "- " (:name %) " [hp:" (:hp %) "]") enemies))
+  (println "Enemies start to surround you - be prepared!\n"))
+
 (defn fight [player enemy]
   (println "Press any key to attack the enemy")
   (read-line)
@@ -56,17 +85,19 @@
       (enemy-died enemy)
       player)))
 
-(defn generate-fight [player]
+(defn start-fight [player enemies]
   (println "=============================================")
-  (let [enemy (random-enemy)]
-    (println (str "You have encountered an " (:name enemy) "!\n"))
-    (show-health player enemy)
+  (player-won? player enemies)
+  (let [enemy (first enemies)]
+    (show-fight-info player enemy)
     (if-let [player (fight player enemy)]
-      (recur player))))
+      (recur player (pop enemies)))))
 
-(defn run-game []
-  (generate-fight player))
+(defn run-game [enemy-count]
+  (let [enemies (generate-enemies enemy-count)]
+    (show-intro enemies)
+    (start-fight player enemies)))
 
-(defn -main
-  [& args]
-  (run-game))
+(defn -main [& args]
+  (let [enemy-count (or (Integer/parseInt (first args)) 5)]
+    (run-game enemy-count)))
